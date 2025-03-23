8e999afa5de1d54cbc199920f8f6f820b5ca515a
aasimdev <aasimdev69@gmail.com>
2025-03-23 16:39:28 +0500

    fixed now

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -3,11 +3,7 @@ import React from "react";
 export const Home = () => {
   return (
     <div className="h-screen max-w-[400px] mx-auto w-full flex justify-center items-center overflow-hidden relative">
-      {/* Top Blur Overlay */}
-
-      {/* Bottom Blur Overlay */}
-
-      {/* Scrolling Container */}
+    
       <div className="marquee-wrapper">
       <div className="absolute top-[-30px] left-0 w-full h-32 bg-white blur-lg z-10 pointer-events-none"></div>
 
@@ -17,7 +13,6 @@ export const Home = () => {
               Market research
             </button>
           ))}
-          {/* Duplicate items for smooth looping */}
           {Array.from({ length: 12 }).map((_, index) => (
             <button key={`duplicate-${index}`} className="marquee-item">
               Market research
